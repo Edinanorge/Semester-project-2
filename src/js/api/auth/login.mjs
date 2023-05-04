e8e6ea7a7f5b3a6c24b43dc485bcfa9bb93e85bf
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-05-04 10:12:59 +0200

    Adds login fixes

--- src/js/api/auth/login.mjs
+++ src/js/api/auth/login.mjs
@@ -1,6 +1,5 @@
 import { BASE_URL } from "../constants.mjs";
 import * as storage from "../../storage/index.mjs";
-import { displayMessage } from "../helpers..mjs";
 
 export async function login(email, password) {
   try {
@@ -14,18 +13,12 @@ export async function login(email, password) {
 
     if (response.ok) {
       const { accessToken, ...user } = await response.json();
-      console.log(accessToken);
       storage.save("token", accessToken);
       storage.save("user", user);
-      displayMessage("loginFeedback", "Success", "success");
-      setTimeout(() => {
-        location.assign("/profile");
-      }, 200);
     } else {
-      const data = await response.json();
-      displayMessage("loginFeedback", data.errors[0].message, "error");
+      throw new Error("Incorrect username or password.");
     }
   } catch (error) {
-    throw new Error(response.errors[0].message);
+    throw error;
   }
 }
--- src/js/handlers/login.mjs
+++ src/js/handlers/login.mjs
@@ -1,16 +1,22 @@
 import { login } from "../api/auth/login.mjs";
+import { displayMessage } from "../api/helpers..mjs";
 
 export function submitLoginForm() {
   const form = document.querySelector("#loginForm");
   if (form) {
-    form.addEventListener("submit", (e) => {
+    form.addEventListener("submit", async (e) => {
       e.preventDefault();
       const form = e.target;
       const formData = new FormData(form);
       const { email, password } = Object.fromEntries(formData.entries());
-
-      login(email, password);
-      form.reset();
+      try {
+        await login(email, password);
+        setTimeout(() => {
+          location.href(`/profile?name=${email}`);
+        }, 300);
+      } catch (error) {
+        displayMessage("loginFeedback", error.message, "error");
+      }
     });
   }
 }
