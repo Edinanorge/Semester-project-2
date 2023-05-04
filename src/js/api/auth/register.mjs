832339a19da264769ad32c9819b10a0940545bdc
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-05-04 10:13:50 +0200

    Adds register fixes

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -1,5 +1,4 @@
 import { BASE_URL } from "../constants.mjs";
-import { displayMessage } from "../helpers..mjs";
 
 export async function register(name, email, password, avatar) {
   try {
@@ -12,17 +11,13 @@ export async function register(name, email, password, avatar) {
     });
 
     if (response.ok) {
-      displayMessage("registerFeedback", "Profile successfully registered.", "success");
-      setTimeout(() => {
-        location.assign("/profile/login");
-      }, 3000);
-      console.log(await response.json());
       return await response.json();
     } else {
-      const data = await response.json();
-      displayMessage("registerFeedback", data.errors[0].message, "error");
+      throw new Error(
+        "Sorry, we were unable to register your account. Please check your email and password and try again."
+      );
     }
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 }
--- src/js/handlers/register.mjs
+++ src/js/handlers/register.mjs
@@ -1,17 +1,23 @@
 import { register } from "../api/auth/register.mjs";
+import { displayMessage } from "../api/helpers..mjs";
 
 export function submitRegisterForm() {
   const form = document.querySelector("#registerForm");
 
   if (form) {
-    form.addEventListener("submit", (e) => {
+    form.addEventListener("submit", async (e) => {
       e.preventDefault();
       const form = e.target;
       const formData = new FormData(form);
       const { name, email, password, avatar } = Object.fromEntries(formData.entries());
-
-      register(name, email, password, avatar);
-      form.reset();
+      try {
+        await register(name, email, password, avatar);
+        setTimeout(() => {
+          location.assign("/profile/login");
+        }, 300);
+      } catch (error) {
+        displayMessage("registerFeedback", error.message, "error");
+      }
     });
   }
 }
