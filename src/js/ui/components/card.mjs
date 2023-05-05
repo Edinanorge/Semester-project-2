e8e5c23a3e53e7d1a727e713aa99c16a7d2f8aab
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-05-05 12:01:24 +0200

    Adds card template

---
+++ src/js/ui/components/card.mjs
@@ -0,0 +1,62 @@
+function singleCardTemplate(listing) {
+  const cardLink = document.createElement("a");
+  cardLink.classList.add("text-dark", "text-decoration-none", "col", "listings-card");
+  cardLink.href = `/listings/single-listing?id=${listing.id}`;
+
+  const card = document.createElement("div");
+  card.classList.add("card", "m-auto", "border-0", "shadow", "position-relative");
+  card.style.width = "18rem";
+
+  const img = document.createElement("img");
+  img.src = listing.media[0];
+  img.classList.add("card-img-top", "img-fluid");
+  img.style.height = "10rem";
+  img.alt = listing.title;
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = listing.title;
+
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("position-absolute", "top-0", "end-0", "bg-primary");
+
+  const cardDate = document.createElement("p");
+  const endDate = new Date(listing.endsAt);
+  const today = new Date();
+  const timeDiff = endDate.getTime() - today.getTime();
+  const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  cardDate.classList.add("text-white", "fw-semibold", "p-2", "m-0");
+  cardDate.textContent = `${daysLeft} days left`;
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer", "d-flex", "justify-content-between");
+
+  const btnDelete = document.createElement("button");
+  btnDelete.classList.add("btn");
+  btnDelete.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+
+  const btnEdit = document.createElement("button");
+  btnEdit.classList.add("btn");
+  btnEdit.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
+
+  cardContent.append(cardDate);
+  cardBody.append(cardTitle, cardContent);
+  cardFooter.append(btnEdit, btnDelete);
+  card.append(img, cardBody, cardFooter);
+  cardLink.appendChild(card);
+
+  return cardLink;
+}
+
+export function renderSingelCardTemplate(listing, parent) {
+  parent.innerHTML = "";
+  parent.append(singleCardTemplate(listing));
+}
+
+export function renderSingelCardTemplates(listingList, parent) {
+  parent.innerHTML = "";
+  parent.append(...listingList.map(singleCardTemplate));
+}
