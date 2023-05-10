d373a6b9b8014216d5e594523d7485f81865c2be
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-05-10 12:16:11 +0200

    Adds listeners

--- src/js/handlers/addListing.mjs
+++ src/js/handlers/addListing.mjs
@@ -1,5 +1,5 @@
 import { addListing } from "../api/listings/addListing.mjs";
-import { displayMessage } from "../api/helpers..mjs";
+import { displayMessage } from "../utility/message.mjs";
 
 export function submitAddListingForm() {
   const form = document.querySelector("#addListingForm");
@@ -10,7 +10,6 @@ export function submitAddListingForm() {
       const form = e.target;
       const formData = new FormData(form);
       const listing = Object.fromEntries(formData.entries());
-      console.log(listing);
 
       addListing(listing);
       displayMessage("createListingFeedback", "Thanks, your product is on sale.", "succes");
