db23f0f5c2dd080168d83c8e7cf39002c437fab6
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-05-19 09:24:39 +0200

    Adds updated hero

--- src/js/api/listings/getListing.mjs
+++ src/js/api/listings/getListing.mjs
@@ -35,6 +35,26 @@ export async function getActiveListings() {
   }
 }
 
+export async function getEndingSoonListings() {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/listings?_seller=true&_bids=true&_active=true&sort=endsAt&sortOrder=asc&limit=8`,
+      {
+        method: "GET",
+        headers: headers(),
+      }
+    );
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    throw new Error("Network error: Unable to fetch listings data.");
+  }
+}
+
 export async function getListing(id) {
   try {
     if (!id) {
